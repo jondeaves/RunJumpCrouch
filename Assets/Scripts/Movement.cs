cedcf80ac434792f4f265fd3d0589861117d937a
Jon Deaves <hi@jondeaves.me>
2023-03-07 00:19:08 -0500

    improvements to movement and animations

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,104 +3,160 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement: MonoBehaviour
+public enum PlayerState
 {
+    Running,
+    Crouching,
+    Jumping,
+    Falling,
+}
+
+[RequireComponent( typeof( CharacterController ) )]
+public class Movement : MonoBehaviour
+{
+    [Tooltip( "How fast player moves during regular movement" )]
+    [SerializeField]
+    private float m_RunSpeed = 4f;
+
+    [Tooltip( "How fast player moves while crouched" )]
+    [SerializeField]
+    private float m_CrouchSpeed = 2f;
+
+    [Tooltip( "How far up player jumps" )]
+    [SerializeField]
+    private float m_JumpPower = 4f;
+
+    private CharacterController m_CharacterController;
     private Animator m_Animator;
-    private Rigidbody m_Rigidbody;
-    private Collider m_Collider;
-    private CapsuleCollider m_CapsuleCollider;
+    private PlayerState m_PlayerState = PlayerState.Running;
 
-    private float m_DistToGround;
-
-    // TODO: Update to check for actual collision?
-    public Boolean IsGrounded { get { return Physics.Raycast(transform.position, -Vector3.up, m_DistToGround - 0.55f); } }
-    
-    private Boolean m_IsCrouched = false;
+    private Vector3 m_MoveDirection = Vector3.zero;
     private Boolean m_PreviousGroundedState;
 
-
-    private float m_ColliderCentreStanding = 0.88f;
+    private float m_ColliderCentreStanding = 0.9f;
     private float m_ColliderHeightStanding = 1.8f;
 
-    private float m_ColliderCentreCrouched = 0.46f;
-    private float m_ColliderHeightCrouched = 0.9f;
+    private float m_ColliderCentreCrouched = 0.6f;
+    private float m_ColliderHeightCrouched = 1.1f;
 
-// Start is called before the first frame update
+    private float m_ColliderCentreJumping = 1f;
+    private float m_ColliderHeightJumping = 1.7f;
+
+    private float m_JumpTime = 0.4f;
+    private float m_JumpTimer = 0f;
+
     void Start()
     {
+        m_CharacterController = GetComponent<CharacterController>();
         m_Animator = GetComponent<Animator>();
-        m_Rigidbody = GetComponent<Rigidbody>();
-        m_Collider = GetComponent<Collider>();
-        m_CapsuleCollider = GetComponent<CapsuleCollider>();
 
-        m_DistToGround = m_Collider.bounds.extents.y;
-        m_PreviousGroundedState = IsGrounded;
+        m_PreviousGroundedState = m_CharacterController.isGrounded;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IsGrounded && !m_PreviousGroundedState)
-        {
-            m_Animator.SetTrigger("Land");
-        }
+        UpdateState();
+        UpdateCollider();
+        UpdateMovement();
 
-        UpdateAnimations();
-
-        m_PreviousGroundedState = IsGrounded;
+        m_PreviousGroundedState = m_CharacterController.isGrounded;
     }
 
-    private void FixedUpdate()
+    private void UpdateMovement()
     {
-        m_Rigidbody.velocity = new Vector3(
-            (m_IsCrouched ? 100f : 200f) * Time.deltaTime,
-            m_Rigidbody.velocity.y,
-            m_Rigidbody.velocity.z
-            );
-        //m_Rigidbody.velocity = (m_IsCrouched ? 100f : 200f) * Time.deltaTime * new Vector3(1f, 0, 0);
+        float xMoveSpeed = m_RunSpeed;
+        if ( m_PlayerState == PlayerState.Jumping )
+            xMoveSpeed = m_RunSpeed * 1.5f;
+        else if ( m_PlayerState == PlayerState.Crouching )
+            xMoveSpeed = m_CrouchSpeed;
+
+        m_MoveDirection.x = xMoveSpeed;
+
+        if ( m_PlayerState != PlayerState.Jumping )
+        {
+            m_MoveDirection.y = Math.Clamp( m_MoveDirection.y + (Physics.gravity.y * Time.deltaTime * 2f), Physics.gravity.y, 9999f );
+        }
+
+        m_CharacterController.Move( m_MoveDirection * Time.deltaTime );
     }
 
-    private void UpdateAnimations()
+    private void UpdateState()
     {
-        if (Input.GetKeyUp(KeyCode.UpArrow) && IsGrounded)
+        // If jumping
+        if ( m_PlayerState == PlayerState.Jumping )
         {
-            m_Animator.SetTrigger("Jump");
-            m_Rigidbody.AddForce(Vector3.up * 250f);
+            m_JumpTimer += Time.deltaTime;
 
-            UpdateCollider(false);
+            if ( m_JumpTimer >= m_JumpTime )
+            {
+                m_JumpTimer = 0f;
+                m_PlayerState = PlayerState.Falling;
+            }
+
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) )
+
+        // If we aren't grounded (i.e. jumping or falling) then we don't want to be able to do anything
+        if ( !m_CharacterController.isGrounded && m_PlayerState != PlayerState.Crouching && m_PlayerState != PlayerState.Jumping )
         {
-            m_Animator.SetTrigger("Crouch");
-            UpdateCollider(true);
-            m_IsCrouched = true;
+            m_Animator.SetTrigger( "Falling" );
+            m_PlayerState = PlayerState.Falling;
+
+            return;
         }
-        else if (Input.GetKeyUp(KeyCode.DownArrow))
+
+        // If we have just become grounded, time to land
+        if ( m_CharacterController.isGrounded && m_PlayerState == PlayerState.Falling )
+        {
+            m_Animator.SetTrigger( "Land" );
+            m_PlayerState = PlayerState.Running;
+        }
+
+        if ( Input.GetKeyUp( KeyCode.UpArrow ) && m_PlayerState == PlayerState.Running )
+        {
+            m_Animator.SetTrigger( "Jump" );
+            m_PlayerState = PlayerState.Jumping;
+            m_MoveDirection.y = m_JumpPower;
+        }
+        else if ( Input.GetKeyDown( KeyCode.DownArrow ) && m_PlayerState == PlayerState.Running )
+        {
+            m_Animator.SetTrigger( "Crouch" );
+            m_PlayerState = PlayerState.Crouching;
+        }
+        else if ( Input.GetKeyUp( KeyCode.DownArrow ) && m_PlayerState == PlayerState.Crouching )
         {
             // Finish crouch
-            m_Animator.SetTrigger("StopCrouch");
-            UpdateCollider(false);
-            m_IsCrouched = false;
+            m_Animator.SetTrigger( "StopCrouch" );
+            m_PlayerState = PlayerState.Running;
         }
     }
 
-    private void UpdateCollider(bool isCrouched)
+    private void UpdateCollider()
     {
-        Debug.Log(string.Format("Is crouched: {0}", isCrouched ? "Yes" : "No"));
-        if (isCrouched)
+        switch ( m_PlayerState )
         {
-            m_CapsuleCollider.height = m_ColliderHeightCrouched;
-            m_CapsuleCollider.center = new Vector3(0, m_ColliderCentreCrouched, 0);
-        } else
-        {
-            m_CapsuleCollider.height = m_ColliderHeightStanding;
-            m_CapsuleCollider.center = new Vector3(0, m_ColliderCentreStanding, 0);
+            case PlayerState.Running:
+                m_CharacterController.height = m_ColliderHeightStanding;
+                m_CharacterController.center = new Vector3( 0, m_ColliderCentreStanding, 0 );
+                break;
+            case PlayerState.Crouching:
+                m_CharacterController.height = m_ColliderHeightCrouched;
+                m_CharacterController.center = new Vector3( 0, m_ColliderCentreCrouched, 0 );
+                break;
+            case PlayerState.Jumping:
+            case PlayerState.Falling:
+                m_CharacterController.height = m_ColliderHeightJumping;
+                m_CharacterController.center = new Vector3( 0, m_ColliderCentreJumping, 0 );
+                break;
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        Debug.Log(other.tag);
+        if ( hit.gameObject.tag.Equals( "Obstacle" ) )
+        {
+            Debug.Log( "Hit object, ouch!" );
+        }
     }
 
     public void FootL() { }
